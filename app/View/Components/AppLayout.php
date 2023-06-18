eb46bbcd8f1000b618c40b41749e86f15341106e
Thilanka Gunawardana <131427047+ThilankaGun@users.noreply.github.com>
2023-06-18 17:46:42 +0530

    revert _construct function after breeze

--- app/View/Components/AppLayout.php
+++ app/View/Components/AppLayout.php
@@ -2,37 +2,35 @@
 
 namespace App\View\Components;
 
-use Closure;
+
 use App\Models\Category;
+use Illuminate\Support\Collection;
 use Illuminate\View\Component;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Contracts\View\View;
 
 class AppLayout extends Component
 {
+    public Collection $categories;
     /**
      * Create a new component instance.
      */
     public function __construct(public ?string $metaTitle = null, public ?string $metaDescription = null)
     {
-        //
+        $this->categories = Category::query()
+            ->join('category_post', 'categories.id', '=', 'category_post.category_id')
+            ->select('categories.title', 'categories.slug', DB::raw('count(*) as total'))
+            ->groupBy('categories.id')
+            ->orderByDesc('total')
+            ->limit(5)
+            ->get();
     }
 
     /**
      * Get the view / contents that represent the component.
      */
-    public function render(): View|Closure|string
+    public function render(): View
     {
-
-        $query = Category::query()
-            ->join('category_post', 'categories.id', '=', 'category_post.category_id')
-            ->select('categories.title', 'categories.slug', DB::raw('count(*) as total'))
-            ->groupBy('categories.id')
-            ->orderByDesc('total')
-            ->limit(5);
-
-        $categories = $query
-            ->get();
-        return view('layouts.app', compact('categories'));
+        return view('layouts.app');
     }
 }
